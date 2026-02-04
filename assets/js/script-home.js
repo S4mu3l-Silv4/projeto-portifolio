d7339be00efe062c41cdceb74fd3bbc2b401e62d
S4mu3l-Silv4 <samuelssone02@gmail.com>
2026-02-04 20:42:50 -0300

    teste

---
+++ assets/js/script-home.js
@@ -0,0 +1,16 @@
+// Nav bar com scroll inteligente:
+
+    let ultimoScrollTop = 0
+    const navBar = document.getElementById("nav-bar")
+
+    window.addEventListener("scroll", function () {
+        const scrollTop = window.pageYOffset || document.documentElement.scrollTop
+
+        if (scrollTop > ultimoScrollTop) {
+            navBar.style.top = "-36px"
+        } else {
+            navBar.style.top = "0"
+        }
+
+        ultimoScrollTop = scrollTop <= 0 ? 0 : scrollTop
+    })
