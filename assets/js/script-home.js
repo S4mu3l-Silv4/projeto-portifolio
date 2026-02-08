42d7dbfa111140d0eaaf38e3275623605e8abb42
S4mu3l-Silv4 <samuelssone02@gmail.com>
2026-02-07 23:45:23 -0300

    teste

--- assets/js/script-home.js
+++ assets/js/script-home.js
@@ -7,7 +7,7 @@
       const scrollTop = window.pageYOffset || document.documentElement.scrollTop
 
       if (scrollTop > ultimoScrollTop) {
-          navBar.style.top = "-36px"
+          navBar.style.top = "-40px"
       } else {
           navBar.style.top = "0"
       }
@@ -17,35 +17,13 @@
 
 // Associação entre links da nav bar e seções:
 
-  const botao = document.getElementById('botao-tema');
-  const body = document.body;
-
-  const temasalvo = localStorage.getItem('tema');
-  temaEscuro(temasalvo === 'escuro');
-
-  function temaEscuro(tipo) {
-    if (tipo == true) {
-      body.classList.add('escuro');
-      botao.innerHTML = '<i class="fa-solid fa-sun"></i>';
-    } else {
-      body.classList.remove('escuro');
-      botao.innerHTML = '<i class="fa-solid fa-moon"></i>';
-    }
-  }
-
-  botao.addEventListener('click', () => {
-    const isescuro = body.classList.toggle('escuro');
-    temaEscuro(isescuro);
-    localStorage.setItem('tema', isescuro ? 'escuro' : 'claro');
-  });
-
   const navLinks = document.querySelectorAll('#nav-bar ul a.link');
   navLinks.forEach(link => {
     link.addEventListener('click', function(e) {
       e.preventDefault();
       const target = document.querySelector(this.getAttribute('href'));
       if (target) {
-        const headerHeight = document.querySelector('header').offsetHeight;
+        const headerHeight = document.querySelector('.nav-bar').offsetHeight;
         const targetPosition = target.offsetTop - headerHeight - 20;
         window.scrollTo({
           top: targetPosition,
